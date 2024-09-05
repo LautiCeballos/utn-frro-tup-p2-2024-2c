864477591885b52d11d13f8f3ee179dfa12dbacc
Lautaro Ceballos <lautaceba18@gmail.com>
2024-09-04 22:18:31 -0300

    Practica

---
+++ practica js/script.js
@@ -0,0 +1,12 @@
+window.alert("1) Hola mundo")
+
+document.write("2) Hola mundo sapeee <br>")
+
+document.write("3) 3 + 5 = ",3+5,"<br>")
+
+var name1 = prompt("Ingrese su nombre","")
+document.write("4) Hola " + name1,"<br>")
+
+var num1 = Number.parseInt(prompt("Ingrese un nro",""))
+var num2 = Number.parseInt(prompt("Ingrese otro nro",""))
+document.write("5) ",num1,"+",num2,"=",num1 + num2)
