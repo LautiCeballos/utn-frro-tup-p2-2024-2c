fbf1deab3074eec120a38ce581cc7492fa7b8568
Lautaro Ceballos <lautaceba18@gmail.com>
2024-09-16 21:02:35 -0300

    16-9

---
+++ practica js/6ymas.js
@@ -0,0 +1,43 @@
+//6
+
+/*window.alert("Ingrese 2 numeros para conocer cual es mayor y cual menor");
+var num1 = prompt("Ingrese primer numero");
+var num2 = prompt("Ingrese segundo numero");
+
+if (num1 > num2){
+    document.write("El numero ",num1," es el mayor");
+}else{
+    document.write("El numero ",num2," es el mayor");
+}
+*/
+
+//7
+/*
+window.alert("Ingrese 3 numeros para conocer cual es mayor y cual menor");
+var num1 = prompt("Ingrese primer numero");
+var num2 = prompt("Ingrese segundo numero");
+var num3 = prompt("Ingrese tercer numero");
+
+if (num1 > num2 && num>num3){
+    document.write("El numero ",num1," es el mayor");
+}else if (num1<num2  && num2>num3){
+    document.write("El numero ",num2," es el mayor");
+}else if(num3>num1 && num3>num2){
+    document.write("El numero ",num3," es el mayor");
+
+}*/
+
+//8
+
+
+/* window.alert("Ingrese un numero para saber si es divisible por 2");
+var num = prompt("Ingrese el numero");
+
+if (num % 2 == 0){
+    document.write("El numero ",num," es divisible por 2");
+}else{
+    document.write("El numero ",num," NO es divisible por 2");
+} */
+
+//9
+
